a3f48ebe5b52ec34955b8f2609d325fc502a3d4a
SAMEEN-MIRZA <126246465+SAMEEN-MIRZA@users.noreply.github.com>
2023-03-01 15:04:45 +0530

    abcded

---
+++ src/loops/forloops1.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class forloops1 {
+	public static void main(String[] args) {
+		
+		int j=100;
+		while(j>=5) {
+			
+			System.out.println(j);
+			j=j-20;
+		}
+		for(int i=1;i<=5;i++) {
+			System.out.println(i*2);
+		}
+		
+	}
+
+}
